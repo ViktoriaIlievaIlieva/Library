985ab91948384751265370e7a8fc072904b454a4
Viktoria Ilieva <viktoria.ilieva.ilieva@gmail.com>
2022-05-18 15:52:21 +0300

    Styling the wishlist information into a table. Added functionality for adding new wished books.

--- library/wishlist_route.py
+++ library/wishlist_route.py
@@ -1,8 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from library.database import get_connection
 from sqlalchemy.engine import CursorResult
 
 blueprint_wishlist = Blueprint("Wishlist", __name__)
+
+
+@blueprint_wishlist.route("/wishlist-add", methods=["GET", "POST"])
+def wishlist_add():
+    if request.method == "GET":
+        return render_template("new_book_for_wishlist.html")
+    else:
+        book = request.form["book"]
+        author = request.form["author"]
+        connection = get_connection()
+        connection.execute("""
+        INSERT INTO "Wishlists"(Name, Author)
+        VALUES (?,?)
+        """, book, author)
+
+    connection.close()
+
+    return redirect("/wishlist")
 
 @blueprint_wishlist.route("/wishlist")
 def wishlist():
@@ -13,6 +31,7 @@ def wishlist():
     FROM Wishlists
     """)
     all_wished_books: list[tuple] = cursor_to_result.fetchall()
+    connection.close()
 
     list_with_dict_for_books_and_authors: list = []
 
