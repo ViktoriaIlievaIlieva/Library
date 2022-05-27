2a881141e0400ac2e4f06c760d817e59db944560
Viktoria Ilieva <viktoria.ilieva.ilieva@gmail.com>
2022-05-27 18:24:40 +0300

    all_authors.html - added query for author_name Created new blueprint_authors.route("/single_author") and single_author.html in authors template directory. In single_author.html - added links to pages for single_book information

--- library/authors_route.py
+++ library/authors_route.py
@@ -4,6 +4,7 @@ from sqlalchemy.engine import CursorResult
 
 blueprint_authors = Blueprint("Authors", __name__)
 
+
 @blueprint_authors.route("/all_authors")
 def all_authors():
     with get_connection() as connection:
@@ -15,3 +16,26 @@ def all_authors():
         list_with_authors: list = all_authors_to_cursor.mappings().all()
 
         return render_template("authors/all_authors.html", list_with_dict_with_authors=list_with_authors)
+
+
+@blueprint_authors.route("/single_author")
+def single_author():
+    id = request.args["id"]
+    author_name = request.args["author_name"]
+    with get_connection() as connection:
+
+        single_authors_to_cursor: CursorResult = connection.execute("""
+        SELECT Books.NameBG AS "title", Books.ID AS "book_id", Related.Name AS "name_of_series", 
+        RelationTypes.Type AS "type_of_series"
+        FROM Books
+        JOIN Authors ON Books.AuthorID = AuthorID
+        JOIN Related ON Books.RelatedID = Related.ID
+        JOIN RelationTypes ON RelationTypes.ID = Related.RelationTypeID
+        WHERE AuthorID = ?
+        """, id)
+
+        list_with_dict_with_authors_books: list = single_authors_to_cursor.mappings().all()
+
+        return render_template("authors/single_author.html", list_with_dict_with_authors_books=list_with_dict_with_authors_books,
+                               author_id=id, author_name=author_name)
+
