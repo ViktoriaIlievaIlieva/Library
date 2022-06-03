8ecfaffbf697c9f74d3dbbbb01116664fd31ff49
Viktoria Ilieva <viktoria.ilieva.ilieva@gmail.com>
2022-06-03 21:23:13 +0300

    Converted books page to use mappings() instead of tuples. Author name in all books is linked to single_author

--- library/books_route.py
+++ library/books_route.py
@@ -9,27 +9,15 @@ blueprint_books = Blueprint("Books", __name__)
 def my_books():
     with get_connection() as connection:
         cursor_to_result: CursorResult = connection.execute("""
-       SELECT Books.NameBG AS "Заглавие", Books.NameENG AS "Title", Authors.Name AS "Автор", IIF(Books.Read, "прочетена", 
-       "нечетена") AS "Статус", Books.ID
+       SELECT Books.NameBG AS "bg_title", Books.NameENG AS "eng_title", Authors.Name AS "author_name",
+        Books.AuthorID AS "author_id", IIF(Books.Read, "прочетена", "нечетена") AS "read", Books.ID AS "book_id"
        FROM Books
        JOIN Authors ON Authors.ID = Books.AuthorID 
        """)
 
-        books_info: list = cursor_to_result.fetchall()
+        books_info: list[dict] = cursor_to_result.mappings().all()
 
-    list_with_dict_book_info: list = []
-
-    for book_info in books_info:
-        bg_name = book_info[0]
-        eng_name = book_info[1]
-        author = book_info[2]
-        read = book_info[3]
-        id = book_info[4]
-
-        book_info: dict = {"bg_name": bg_name, "eng_name": eng_name, "author": author, "read": read, "id": id}
-        list_with_dict_book_info.append(book_info)
-
-    return render_template("mybooks/all_books.html", books_info=list_with_dict_book_info)
+    return render_template("mybooks/all_books.html", list_with_dict_with_books_info=books_info)
 
 
 @blueprint_books.route("/single_book")
